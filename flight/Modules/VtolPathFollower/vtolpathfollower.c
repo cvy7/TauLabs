b61fea75e2d043d925bc098c442f1ac1d9c6a459
Daniel Strnad <strnadda@gmail.com>
2014-07-16 18:12:24 +0200

    VtolPathFollower: makes RTH altitude and RTH velocity adjustable in settings

--- flight/Modules/VtolPathFollower/vtolpathfollower.c
+++ flight/Modules/VtolPathFollower/vtolpathfollower.c
@@ -127,8 +127,10 @@ static void vtolPathFollowerTask(void *parameters)
 	portTickType lastUpdateTime;
 	
 	VtolPathFollowerSettingsConnectCallback(vtol_follower_control_settings_updated);
+	VtolPathFollowerSettingsConnectCallback(vtol_follower_fsm_settings_updated);
 	AltitudeHoldSettingsConnectCallback(vtol_follower_control_settings_updated);
 	vtol_follower_control_settings_updated(NULL);
+	vtol_follower_fsm_settings_updated(NULL);
 	
 	VtolPathFollowerSettingsGet(&guidanceSettings);
 	
